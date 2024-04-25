8052dd218667946d0e79541443f87d58c5ab5557
LiuYHCode <lyh997835@163.com>
2024-04-25 14:52:51 +0800

    【修改内容】:BigKey测试，项目事务管理配置 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:BigKey测试，项目事务管理配置 【需要测试】:是

--- src/main/java/com/at2024/redis7_study/controller/BigKeyController.java
+++ src/main/java/com/at2024/redis7_study/controller/BigKeyController.java
@@ -2,18 +2,10 @@ package com.at2024.redis7_study.controller;
 
 import com.at2024.redis7_study.service.BigKeyService;
 import io.swagger.annotations.Api;
-import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.exceptions.JedisException;
-import redis.clients.jedis.params.ScanParams;
-import redis.clients.jedis.resps.ScanResult;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author lyh
@@ -38,6 +30,42 @@ public class BigKeyController {
     @GetMapping(value = "/delBigHash")
     public void delBigHash(@RequestParam("host")String host, @RequestParam("port")int port, @RequestParam("password")String password, @RequestParam("bigHashKey")String bigHashKey) {
         bigKeyService.delBigHash(host, port, password, bigHashKey);
+    }
+
+    /**
+     * List删除：ltrim + hdel，先进行修剪，最后进行删除
+     * @param host
+     * @param port
+     * @param password
+     * @param bigListKey
+     */
+    @GetMapping(value = "/delBigList")
+    public void delBigList(@RequestParam("host")String host, @RequestParam("port")int port, @RequestParam("password")String password, @RequestParam("bigListKey")String bigListKey) {
+        bigKeyService.delBigList(host, port, password, bigListKey);
+    }
+
+    /**
+     * Set删除：srem + hdel，先进行修剪，最后进行删除
+     * @param host
+     * @param port
+     * @param password
+     * @param bigSetKey
+     */
+    @GetMapping(value = "/delBigSet")
+    public void delBigSet(@RequestParam("host")String host, @RequestParam("port")int port, @RequestParam("password")String password, @RequestParam("bigSetKey")String bigSetKey) {
+        bigKeyService.delBigSet(host, port, password, bigSetKey);
+    }
+
+    /**
+     * Zset删除：zrem + hdel，先进行修剪，最后进行删除
+     * @param host
+     * @param port
+     * @param password
+     * @param bigZsetKey
+     */
+    @GetMapping(value = "/delBigZset")
+    public void delBigZset(@RequestParam("host")String host, @RequestParam("port")int port, @RequestParam("password")String password, @RequestParam("bigZsetKey")String bigZsetKey) {
+        bigKeyService.delBigZset(host, port, password, bigZsetKey);
     }
 
 }
--- src/main/java/com/at2024/redis7_study/service/BigKeyService.java
+++ src/main/java/com/at2024/redis7_study/service/BigKeyService.java
@@ -1,14 +1,15 @@
 package com.at2024.redis7_study.service;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.exceptions.JedisException;
 import redis.clients.jedis.params.ScanParams;
 import redis.clients.jedis.resps.ScanResult;
+import redis.clients.jedis.resps.Tuple;
 
 import java.util.List;
 import java.util.Map;
@@ -24,7 +25,9 @@ public class BigKeyService {
 
     public void delBigHash(String host, int port, String password, String bigHashKey) {
         Jedis jedis = new Jedis(host, port);
-        jedis.auth(password);
+        if (StringUtils.isNotEmpty(password)) {
+            jedis.auth(password);
+        }
 
         //分批删除
         try {
@@ -32,6 +35,7 @@ public class BigKeyService {
             ScanParams scanParams = new ScanParams().count(100);
             String cursor = "";
             while (!cursor.equals("0")){
+                //这里的操作就和redis客户端登录之后，scan cursor MATCH * count 100一样
                 ScanResult<Map.Entry<String, String>> scanResult = jedis.hscan(bigHashKey, cursor, scanParams);
                 List<Map.Entry<String, String>> entryList = scanResult.getResult();
 
@@ -42,8 +46,6 @@ public class BigKeyService {
                     }
                 }
                 long t2 = System.currentTimeMillis();
-
-//                LOGGER.info("删除"+ entryList.size() + "条数据，耗时: " + (t2-t1) + "毫秒,cursor:" + cursor);
                 LOGGER.info(String.format("删除%d条数据，耗时: %lf毫秒,cursor:%s", entryList.size(), (t2-t1), cursor));
 
                 //返回0说明遍历完成
@@ -61,4 +63,106 @@ public class BigKeyService {
             }
         }
     }
+
+    public void delBigList(String host, int port, String password, String bigListKey) {
+        Jedis jedis = new Jedis(host, port);
+        if (StringUtils.isNotEmpty(password)) {
+            jedis.auth(password);
+        }
+
+        long llen = jedis.llen(bigListKey);
+        int count = 0;
+        int left = 100;
+        while (count < llen) {
+            //每次从左侧截掉100个
+            jedis.ltrim(bigListKey, left, llen);
+            count += left;
+        }
+
+        //最终将key进行删除
+        jedis.del(bigListKey);
+    }
+
+    public void delBigSet(String host, int port, String password, String bigSetKey) {
+        Jedis jedis = new Jedis(host, port);
+        if (StringUtils.isNotEmpty(password)) {
+            jedis.auth(password);
+        }
+
+        //分批删除
+        try {
+            //每次删除100条
+            ScanParams scanParams = new ScanParams().count(100);
+            String cursor = "";
+            while (!cursor.equals("0")){
+                //这里的操作就和redis客户端登录之后，scan cursor MATCH * count 100一样
+                ScanResult<String> scanResult = jedis.sscan(bigSetKey, cursor, scanParams);
+                List<String> memberList = scanResult.getResult();
+
+                long t1 = System.currentTimeMillis();
+                if (CollectionUtils.isNotEmpty(memberList)) {
+                    for (String member : memberList) {
+                        jedis.srem(bigSetKey, member);
+                    }
+                }
+                long t2 = System.currentTimeMillis();
+                LOGGER.info(String.format("删除%d条数据，耗时: %lf毫秒,cursor:%s", memberList.size(), (t2-t1), cursor));
+
+                //返回0说明遍历完成
+                cursor = scanResult.getCursor();
+            }
+
+            //删除bigKey
+            jedis.del(bigSetKey);
+        }catch (JedisException e){
+            e.printStackTrace();
+            LOGGER.error(e.getMessage(),e);
+        }finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+    }
+
+    public void delBigZset(String host, int port, String password, String bigZsetKey) {
+        Jedis jedis = new Jedis(host, port);
+        if (StringUtils.isNotEmpty(password)) {
+            jedis.auth(password);
+        }
+
+        //分批删除
+        try {
+            //每次删除100条
+            ScanParams scanParams = new ScanParams().count(100);
+            String cursor = "";
+            while (!cursor.equals("0")){
+                //这里的操作就和redis客户端登录之后，scan cursor MATCH * count 100一样
+                ScanResult<Tuple> scanResult = jedis.zscan(bigZsetKey, cursor, scanParams);
+                List<Tuple> tupleList = scanResult.getResult();
+
+                long t1 = System.currentTimeMillis();
+                if (CollectionUtils.isNotEmpty(tupleList)) {
+                    for (Tuple tuple : tupleList) {
+                        jedis.zrem(bigZsetKey, tuple.getElement());
+                    }
+                }
+                long t2 = System.currentTimeMillis();
+                LOGGER.info(String.format("删除%d条数据，耗时: %lf毫秒,cursor:%s", tupleList.size(), (t2-t1), cursor));
+
+                //返回0说明遍历完成
+                cursor = scanResult.getCursor();
+            }
+
+            //删除bigKey
+            jedis.del(bigZsetKey);
+        }catch (JedisException e){
+            e.printStackTrace();
+            LOGGER.error(e.getMessage(),e);
+        }finally {
+            if(jedis != null){
+                jedis.close();
+            }
+        }
+    }
+
 }
