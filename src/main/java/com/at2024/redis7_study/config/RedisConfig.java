196bbdab359f488ab2da6d461b446187c8c9967f
LiuYHCode <lyh997835@163.com>
2024-06-08 18:54:10 +0800

    【修改内容】:聚划算以及redisTemplate序列化配置优化 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:聚划算以及redisTemplate序列化配置优化 【需要测试】:是

--- src/main/java/com/at2024/redis7_study/config/RedisConfig.java
+++ src/main/java/com/at2024/redis7_study/config/RedisConfig.java
@@ -1,5 +1,16 @@
 package com.at2024.redis7_study.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +20,11 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author lyh
@@ -23,17 +39,47 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-        // 使用StringRedisSerializer来序列化和反序列化redis的key值
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        template.setKeySerializer(stringRedisSerializer);
-        template.setValueSerializer(stringRedisSerializer);
 
-        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
-        // Hash的key也采用StringRedisSerializer的序列化方式
+        //使用StringRedisSerializer来序列化和反序列化redis的key值，这里是序列化的工具
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = this.myJasonRedisSerializer();
+
+        //设置key的序列化（Redis默认序列化工具）
+        template.setKeySerializer(stringRedisSerializer);
+        //Hash的key也采用StringRedisSerializer的序列化方式
         template.setHashKeySerializer(stringRedisSerializer);
-        // Hash的key也采用GenericJackson2JsonRedisSerializer的序列化方式
+
+        //设置value的序列化（Jackson序列化工具）
+        template.setValueSerializer(genericJackson2JsonRedisSerializer);
+        // Hash的value也采用GenericJackson2JsonRedisSerializer的序列化方式
         template.setHashValueSerializer(genericJackson2JsonRedisSerializer);
         template.afterPropertiesSet();
         return template;
+    }
+
+    private GenericJackson2JsonRedisSerializer myJasonRedisSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 设置可见性
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+
+        // 序列化后添加类信息(不配置,序列化后就是一个Json字符串)
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+        // 将日期序列化为可读字符串而不是时间戳
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        //设置时间模块(格式化，不设置，则输出默认格式)
+        JavaTimeModule timeModule = new JavaTimeModule();
+        // LocalDateTime
+        timeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.of("Asia/Shanghai"))));
+        timeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.of("Asia/Shanghai"))));
+        // LocalDate
+        timeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.of("Asia/Shanghai"))));
+        timeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.of("Asia/Shanghai"))));
+
+        // 设置自定义时间模块
+        objectMapper.registerModule(timeModule);
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
     }
 }
--- src/main/java/com/at2024/redis7_study/controller/JHSTaskController.java
+++ src/main/java/com/at2024/redis7_study/controller/JHSTaskController.java
@@ -1,13 +1,43 @@
 package com.at2024.redis7_study.controller;
 
+import com.at2024.redis7_study.entities.Product;
+import com.at2024.redis7_study.service.JHSTaskService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Api(tags = "模拟聚划算商品上下架")
 @RestController
 @Slf4j
 public class JHSTaskController {
 
+    public static final String JHS_KEY = "jhs";
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private JHSTaskService jhsService;
+
+    @ApiOperation("聚划算案例，每次1页，每页5条数据")
+    @GetMapping("/product/find")
+    public List<Product> find(int page, int size) {
+        //设置寻找记录的起始和结束位置
+        int start = (page - 1) * size;
+        int end = start + size -1;
+        //1.先去缓存寻找
+        List productList = redisTemplate.opsForList().range(JHS_KEY, start, end);
+        //2.缓存如果存在就直接拿出，如果不存在就去数据库查找
+        if (CollectionUtils.isEmpty(productList)) {
+            productList = jhsService.getProductsFromMysql();
+        }
+        log.info("参加活动的商家: {}", productList);
+        return productList;
+    }
 }
